583674a9d4b6b8c698103b7756c9da2ef60d8d26
shivamk824208 <shivamk824208@gmail.com>
2025-12-11 22:56:48 +0530

    Removed node_modules from Git tracking

--- db.js
+++ db.js
@@ -1,15 +1,24 @@
 const mongoose = require('mongoose');
+// define the mongodb connection url
+//const MONGODB_URL = ''
+require('dotenv').config();
 
-const mongoURL = 'mongodb://localhost:27017/hotels'
 
+
+const mongoURL = process.env.MONGODB_URL;
+console.log("Loaded URI:", mongoURL);
 //set up mongoDB connection
-mongoose.connect(mongoURL)
-.then(()=> console.log('mongodb connected'))
-.catch((err)=> console.log('mongodb connection error', err));
+mongoose.connect(process.env.MONGODB_URL, {
+    serverSelectionTimeoutMS: 10000
+})
+.then(() => console.log("MongoDB connected"))
+.catch(err => console.log("MongoDB connection error:", err));
+
 
 //get the default connection
 //mongoose maintains a default connection object representing the mongoDB connection
 const db = mongoose.connection;
+//console.log("Loaded URI:", process.env.MONGO_URI);
 
 //define event listeners for database connection
 db.on('connected', ()=> {
--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const app = express();
-const port = 3000;
-const db = require('./db');
+//const port = 3000;
+const db = require('./db.js');
+require('dotenv').config();
+
+const PORT = process.env.PORT || 3000;
 
 const bodyParser = require('body-parser');
 app.use(bodyParser.json()); //req.body
@@ -15,12 +18,13 @@ app.get('/', (req, res)=> {
 
 
 
+
 const personRoutes = require('./routes/personRoutes.js');
 app.use('/', personRoutes);
 
 
 const menuRoutes = require('./routes/menuRoutes.js');
 app.use('/', menuRoutes);
-app.listen(port, ()=> {
-    console.log(`server is running on http://localhost:${port}`);
+app.listen(PORT, ()=> {
+    console.log(`server is running on http://localhost:${PORT}`);
 });
